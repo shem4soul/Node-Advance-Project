2547227e6427b776e29624016df3751bb240930a
Shittu Emmanuel <shem4soul@gmail.com>
2026-09-24 14:03:55 +0100

    add path module for handling file paths

---
+++ src/04-path-module.ts
@@ -0,0 +1,32 @@
+// build and read file paths
+
+import path from "node:path";
+
+//const filePath = projectRoot + "/upload" + filename;
+
+//path.join : uses the correct seperator for the current os
+// /users/shem/project/file.txt
+
+// c:\users\shem\project\file.txt
+
+// process.cwd: the folder from where the nnode js was started
+
+const projectRoot = process.cwd();
+
+console.log(projectRoot);
+
+// /uploads/users/42/profile.photo.png
+
+const userId = "42";
+const originalName = "profile.photo.png";
+
+// imp-> path.join -> creates a path string
+// it will not create the folder
+
+const uploadFilePath = path.join(
+  projectRoot,
+  "uploads",
+  "users",
+  userId,
+  originalName,
+);
